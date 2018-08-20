3f71f19f31a85d728bd49685a332dfab53f2c108
Dracaryon <hugo.felypemaciel@hotmail.com>
2018-08-20 04:36:58 -0300

    Início - Telas
    As telas base do app. O Constraint Layout será adicionado e tratado em
    novas atualizações.

---
+++ Telas/app/src/main/java/telas/mobitour/pretcc/com/telas/TelaHistoriasDetalheActivity.java
@@ -0,0 +1,13 @@
+package telas.mobitour.pretcc.com.telas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaHistoriasDetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_tela_historias_detal);
+    }
+}
